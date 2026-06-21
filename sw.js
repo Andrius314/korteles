d1f4766148e3e178d03ce8a23e3708379cd0bf79
Andrius314 <206417124+Andrius314@users.noreply.github.com>
2026-06-21 21:50:23 +0300

    Add shared card status database and offline support

---
+++ sw.js
@@ -0,0 +1,56 @@
+const CACHE_NAME = 'nuolaidu-korteles-v4';
+const OFFLINE_ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './img/iki.jpg',
+  './img/iki_logo.png',
+  './img/maxima.jpg',
+  './img/maxima_logo.png',
+  './img/lidl.jpg',
+  './img/lidl_logo.png',
+  './img/norfa.jpg',
+  './img/norfa_logo.png',
+  './img/promo.jpg',
+  './img/promo_logo.png',
+  './img/senukai.jpg',
+  './img/senukai_logo.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(OFFLINE_ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      ))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      const fromNetwork = fetch(event.request)
+        .then((response) => {
+          if (response.ok && new URL(event.request.url).origin === self.location.origin) {
+            const copy = response.clone();
+            caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+          }
+          return response;
+        })
+        .catch(() => cached || caches.match('./index.html'));
+
+      return cached || fromNetwork;
+    })
+  );
+});
